da9737cfe45b2d2b728ee97c0d80ef2f8ab180bb
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-12 15:18:03 +0300

    chore: improve JSDoc comments and types

--- src/index.ts
+++ src/index.ts
@@ -48,11 +48,42 @@ export interface SessionOptions<
 	Data = unknown,
 	Key extends string | undefined = "session",
 > {
+	/**
+	 * The key that will be added to the context
+	 * @default "session"
+	 * */
 	key?: Key;
+	/**
+	 * The {@link Storage} in which to store the session
+	 *
+	 * [Documentation](https://gramio.netlify.app/storages/)
+	 */
 	storage?: Storage;
+	/**
+	 * A function that allows you to specify which key will be used to identify the session in the storage
+	 * @default (context) => `${context.senderId}`
+	 */
 	getSessionKey?: (
 		context: ContextType<BotLike, Events>,
 	) => MaybePromise<string>;
+	/** Specify which data will exist while there has not been an record in the repository yet
+	 *
+	 * To **type** a session data, you need to specify the type as the `ReturnType` of the initial function.
+	 *
+	 * ```ts
+	 * interface MySessionData {
+	 *     apple: number;
+	 *     some?: "maybe-empty";
+	 * }
+	 *
+	 * bot.extend(
+	 *     session({
+	 *         key: "sessionKey",
+	 *         initial: (): MySessionData => ({ apple: 1 }),
+	 *     })
+	 * );
+	 * ```
+	 */
 	initial?: (context: ContextType<BotLike, Events>) => MaybePromise<Data>;
 }
 
@@ -106,7 +137,90 @@ function createProxy<T>(
  */
 export function session<Data = unknown, Key extends string = "session">(
 	options: SessionOptions<Data, Key> = {},
-) {
+): Plugin<
+	// biome-ignore lint/complexity/noBannedTypes: Temporally fix https://jsr.io/@gramio/session/0.1.2/src/index.ts#L109 slow-types-compiler issue
+	{},
+	import("gramio").DeriveDefinitions & {
+		message: Awaited<{ [key in Key extends string ? Key : "session"]: Data }>;
+		callback_query: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		channel_post: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		chat_join_request: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		chosen_inline_result: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		inline_query: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		web_app_data: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		successful_payment: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		video_chat_started: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		video_chat_ended: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		video_chat_scheduled: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		video_chat_participants_invited: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		passport_data: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		new_chat_title: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		new_chat_photo: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		pinned_message: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		poll_answer: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		pre_checkout_query: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		proximity_alert_triggered: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		shipping_query: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		group_chat_created: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		delete_chat_photo: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		location: Awaited<{ [key in Key extends string ? Key : "session"]: Data }>;
+		invoice: Awaited<{ [key in Key extends string ? Key : "session"]: Data }>;
+		message_auto_delete_timer_changed: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		migrate_from_chat_id: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		migrate_to_chat_id: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+		new_chat_members: Awaited<{
+			[key in Key extends string ? Key : "session"]: Data;
+		}>;
+	}
+> {
 	const key = (options.key ?? "session") as Key extends string
 		? Key
 		: "session";
